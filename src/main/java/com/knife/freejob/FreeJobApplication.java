9affa55e43f099414ef9b4cf6d3a5fd6477d177e
habib <habibsiby@gmail.com>
2024-03-19 22:43:12 -0400

    first commit new project

---
+++ src/main/java/com/knife/freejob/FreeJobApplication.java
@@ -0,0 +1,13 @@
+package com.knife.freejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeJobApplication.class, args);
+	}
+
+}
